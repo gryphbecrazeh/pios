f91842c0e4a4b66566461877e389d5e9250ac938
Christopher Cordine <gryphbecrazeh@gmail.com>
2019-08-20 09:51:08 -0400

    added search functions to items.js for server side filtering

--- routes/api/items.js
+++ routes/api/items.js
@@ -81,3 +81,63 @@ router.delete("/:id", auth, (req, res) => {
 });
 
 module.exports = router;
+
+sortByDate = item => {
+	return item.sort((a, b) => {
+		return this.state.sort === false
+			? new Date(a.date) - new Date(b.date)
+			: new Date(b.date) - new Date(a.date);
+	});
+};
+sortByTarget = item => {
+	return item.sort((a, b) => {
+		return this.state.sort === false
+			? a[this.state.sortTarget] - b[this.state.sortTarget]
+			: b[this.state.sortTarget] - a[this.state.sortTarget];
+	});
+};
+onChangeDate = (target, e) => {
+	if (target === "start") {
+		this.setState({ startDate: new Date(e) });
+	} else {
+		this.setState({ endDate: new Date(e) });
+	}
+};
+filterByDateRange = (item, range1, range2) => {
+	let arrangeDates = new Date(range2) > new Date(range1);
+	let start = arrangeDates === true ? new Date(range1) : new Date(range2);
+	let end = arrangeDates === true ? new Date(range2) : new Date(range1);
+	return item.filter(
+		item => new Date(item.date) >= start && new Date(item.date) <= end
+	);
+};
+toggleSort = e => {
+	this.setState({
+		sort: !this.state.sort,
+		sortTarget: e.target.name
+	});
+};
+onChangeSearch = e => {
+	this.setState({
+		searchQuery: e.target.value ? e.target.value : false
+	});
+};
+onChangeSeachCriteria = e => {
+	let critera = this.props.keys.dbKeysList.filter(
+		item => item.value === e.target.value
+	)[0];
+	this.setState({
+		searchTarget: critera.value,
+		searchTargetLabel: critera
+	});
+};
+search = item => {
+	return item.filter(order =>
+		order[this.state.searchTarget].match(
+			new RegExp(
+				`${this.state.searchQuery === false ? ".+" : this.state.searchQuery}`,
+				"gmi"
+			)
+		)
+	);
+};
